a1a3fb4b29616bdef0af8b287d4a40274acc1d68
JLarky <jlarky@gmail.com>
2023-12-11 01:04:50 -0700

    implement list of albums

--- REST/MyPhotoSite/src/config.ts
+++ REST/MyPhotoSite/src/config.ts
@@ -4,3 +4,15 @@ export const scopes = [
 	"https://www.googleapis.com/auth/photoslibrary.readonly",
 	"profile",
 ];
+
+// The number of photos to load for search requests.
+export const photosToLoad = 150;
+
+// The page size to use for search requests. 100 is reccommended.
+export const searchPageSize = 100;
+
+// The page size to use for the listing albums request. 50 is reccommended.
+export const albumPageSize = 50;
+
+// The API end point to use. Do not change.
+export const apiEndpoint = "https://photoslibrary.googleapis.com";
---
+++ REST/MyPhotoSite/src/photos.ts
@@ -0,0 +1,105 @@
+import * as config from "./config";
+
+const logger = {
+	verbose: console.log,
+	debug: console.log,
+	info: console.info,
+	error: console.error,
+};
+
+// Returns a list of all albums owner by the logged in user from the Library
+// API.
+export async function* libraryApiGetAlbums(authToken: string) {
+	let counter = 0;
+	let nextPageToken = null;
+	let error = null;
+
+	let parameters = new URLSearchParams();
+	parameters.append("pageSize", "" + config.albumPageSize);
+
+	try {
+		// Loop while there is a nextpageToken property in the response until all
+		// albums have been listed.
+		do {
+			logger.verbose(`Loading albums. Received so far: ${counter}`);
+			// Make a GET request to load the albums with optional parameters (the
+			// pageToken if set).
+			const albumResponse = await fetch(
+				config.apiEndpoint + "/v1/albums?" + parameters,
+				{
+					method: "get",
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: "Bearer " + authToken,
+					},
+				},
+			);
+
+			const result = await checkStatus<{
+				albums: {
+					id: string;
+					title: string;
+					productUrl: string;
+					mediaItemsCount: string;
+					coverPhotoBaseUrl: string;
+					coverPhotoMediaItemId: string;
+				}[];
+			}>(albumResponse);
+
+			logger.debug(`Response: ${result}`);
+
+			if (result && result.albums) {
+				logger.verbose(`Number of albums received: ${result.albums.length}`);
+				// Parse albums and add them to the list, skipping empty entries.
+				const items = result.albums.filter((x) => !!x);
+
+				counter += items.length;
+
+				yield items;
+			}
+			if (result.nextPageToken) {
+				parameters.set("pageToken", result.nextPageToken);
+			} else {
+				parameters.delete("pageToken");
+			}
+
+			// Loop until all albums have been listed and no new nextPageToken is
+			// returned.
+		} while (parameters.has("pageToken"));
+	} catch (err) {
+		// Log the error and prepare to return it.
+		error = err;
+		logger.error(error);
+	}
+
+	logger.info("Albums loaded.");
+	if (error) throw error;
+}
+
+// Return the body as JSON if the request was successful, or thrown a StatusError.
+async function checkStatus<T>(response: Response): Promise<T> {
+	if (!response.ok) {
+		// Throw a StatusError if a non-OK HTTP status was returned.
+		let message = "";
+		try {
+			// Try to parse the response body as JSON, in case the server returned a useful response.
+			message = await response.json();
+		} catch (err) {
+			// Ignore if no JSON payload was retrieved and use the status text instead.
+		}
+		throw new StatusError(response.status, response.statusText, message);
+	}
+
+	// If the HTTP status is OK, return the body as JSON.
+	return await response.json();
+}
+
+// Custom error that contains a status, title and a server message.
+class StatusError extends Error {
+	constructor(status, title, serverMessage, ...params) {
+		super(...params);
+		this.status = status;
+		this.statusTitle = title;
+		this.serverMessage = serverMessage;
+	}
+}
